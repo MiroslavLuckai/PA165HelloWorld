4f4017e7dc4f29d9c8bc074dfbc5adbbda6cc06d
Miroslav Lučkai <xluckai@fi.muni.cz>
2020-02-18 10:26:45 +0100

    Hello world

---
+++ src/test/java/cz/muni/fi/pa165/AppTest.java
@@ -0,0 +1,20 @@
+package cz.muni.fi.pa165;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
